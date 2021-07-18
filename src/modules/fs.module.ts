3d5e3f6cf7097a5c3620166c86f235d7d1245bd8
FloydCrimson <36471780+FloydCrimson@users.noreply.github.com>
2021-07-18 13:44:50 +0200

    Added FSModule.Cleaner

--- src/modules/fs.module.ts
+++ src/modules/fs.module.ts
@@ -24,8 +24,40 @@ export class FSModule {
 
     // ...
 
-    public async readdir(path: _fs_.PathLike, options: { encoding: BufferEncoding | null; withFileTypes?: false | undefined } | BufferEncoding | undefined | null) {
+    public async lstat(path: _fs_.PathLike, options: _fs_.StatOptions | undefined) {
+        return new Promise<[NodeJS.ErrnoException | null, Transformer<_fs_.Stats | _fs_.BigIntStats>]>((resolve) => _fs_.lstat(path, options, async (err: NodeJS.ErrnoException | null, stats: _fs_.Stats | _fs_.BigIntStats) => resolve([err, await FSModule.StatsTransformer(stats)])));
+    }
+
+    public async readdir(path: _fs_.PathLike, options: { encoding: BufferEncoding | null; } | BufferEncoding | undefined | null) {
         return new Promise<[NodeJS.ErrnoException | null, string[]]>((resolve) => _fs_.readdir(path, options, (err: NodeJS.ErrnoException | null, files: string[]) => resolve([err, files])));
     }
 
+    // HELP
+
+    private static async StatsTransformer(stats: _fs_.Stats | _fs_.BigIntStats) {
+        return FSModule.Cleaner<_fs_.Stats | _fs_.BigIntStats>(stats, ['_checkModeProperty']);
+    }
+
+    private static async Cleaner<T, K extends keyof T = never>(input: T, keys: (keyof any)[] = []): Promise<Transformer<T, K>> {
+        const output = new Object() as unknown;
+        for (const key in input) {
+            if (!keys.includes(key)) {
+                if (typeof input[key] === 'function') {
+                    try {
+                        const result = (input[key] as unknown as <I = any>() => Promise<I> | I)();
+                        const isPromise = typeof result === 'object' && result[key]['then'] === 'function';
+                        output[key] = (isPromise ? await (result as Promise<[any, any]>).then((r) => [r, null]).catch((e) => [null, e]) : [result, null]) as unknown as any;
+                    } catch (error) {
+                        output[key] = [null, error] as unknown as any;
+                    }
+                } else {
+                    output[key] = input[key];
+                }
+            }
+        }
+        return output as Transformer<T, K>;
+    }
+
 }
+
+type Transformer<T, K extends keyof T = never> = { [KT in Exclude<keyof T, K>]: T[KT] extends () => Promise<infer I> | infer I ? [I, any] : never };
